2197876eb3e68cf673cb16843ac6dff2b7a2a6ba
med aissa <50526352+medands@users.noreply.github.com>
2026-06-16 17:50:09 +0100

    Refactor Dialogs.h header guard and function declarations

--- MdPhotoV/src/include/Dialogs.h
+++ MdPhotoV/src/include/Dialogs.h
@@ -7,14 +7,8 @@ void ShowHelpWindow(HWND owner);
 void ShowOptionsDialog(HWND owner);
 
 // Callbacks (declared here but defined in cpp)
-<<<<<<< HEAD
 INT_PTR CALLBACK AboutDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK HelpWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK OptionsDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
-=======
-LRESULT CALLBACK AboutDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK HelpWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK OptionsDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
->>>>>>> 408d8a1537e49d6cc2d3ea5c316d3eadd095fb0c
 
 #endif
